6bedb8f329f10766d0ef26a226d06e7b16f08e7c
Dias Kazhtai <dkazhtai21@mail.ru>
2020-06-17 12:34:12 +0300

    layouts end

--- src/views/Categories.vue
+++ src/views/Categories.vue
@@ -1,79 +1,79 @@
 <template>
-        <div>
+  <div>
     <div class="page-title">
-        <h3>Категории</h3>
+      <h3>Категории</h3>
     </div>
     <section>
-        <div class="row">
+      <div class="row">
         <div class="col s12 m6">
-            <div>
+          <div>
             <div class="page-subtitle">
-                <h4>Создать</h4>
+              <h4>Создать</h4>
             </div>
 
             <form>
-                <div class="input-field">
+              <div class="input-field">
                 <input
                     id="name"
                     type="text"
                 >
                 <label for="name">Название</label>
                 <span class="helper-text invalid">Введите название</span>
-                </div>
+              </div>
 
-                <div class="input-field">
+              <div class="input-field">
                 <input
                     id="limit"
                     type="number"
                 >
                 <label for="limit">Лимит</label>
                 <span class="helper-text invalid">Минимальная величина</span>
-                </div>
+              </div>
 
-                <button class="btn waves-effect waves-light" type="submit">
+              <button class="btn waves-effect waves-light" type="submit">
                 Создать
                 <i class="material-icons right">send</i>
-                </button>
+              </button>
             </form>
-            </div>
+          </div>
         </div>
         <div class="col s12 m6">
-            <div>
+          <div>
             <div class="page-subtitle">
-                <h4>Редактировать</h4>
+              <h4>Редактировать</h4>
             </div>
 
             <form>
-                <div class="input-field" >
+              <div class="input-field" >
                 <select>
-                    <option>Category</option>
+                  <option>Category</option>
                 </select>
                 <label>Выберите категорию</label>
-                </div>
+              </div>
 
-                <div class="input-field">
+              <div class="input-field">
                 <input type="text" id="name">
                 <label for="name">Название</label>
                 <span class="helper-text invalid">TITLE</span>
-                </div>
+              </div>
 
-                <div class="input-field">
+              <div class="input-field">
                 <input
                     id="limit"
                     type="number"
                 >
                 <label for="limit">Лимит</label>
                 <span class="helper-text invalid">LIMIT</span>
-                </div>
+              </div>
 
-                <button class="btn waves-effect waves-light" type="submit">
+              <button class="btn waves-effect waves-light" type="submit">
                 Обновить
                 <i class="material-icons right">send</i>
-                </button>
+              </button>
             </form>
-            </div>
+          </div>
         </div>
-        </div>
+      </div>
     </section>
-    </div>
+  </div>
 </template>
